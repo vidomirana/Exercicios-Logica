3012b4bb1b4866153d9efbce1340b2d9ce2c2651
VITOR MIRANDA SOUSA <sousavitor35@gmail.com>
2023-11-07 10:10:10 -0300

    Correção da chamada de lista crescente
    Nessa o ChatGPT me ajudou a resolver, mudei a função para que ela fizesse as operações de ordenação numa cópia da lista, e não na lista original.

--- PYTHON/ordena_lista.py
+++ PYTHON/ordena_lista.py
@@ -6,17 +6,19 @@ def organizar(lista, ordem):
     :lista: Lista a ser ordenada
     :ordem: ordem d organização, 'c'=crescente (padrão), 'd'=decrescente
     '''
+    lista_copy = lista.copy()  # Cria uma cópia da lista original
+
     if ordem == 'c':
-        for num in range(0, len(lista)):
-            for i in range(num+1, len(lista)):
-                if lista[num] > lista[i]:
-                    lista[i], lista[num] = lista[num], lista[i]
+        for num in range(0, len(lista_copy)):
+            for i in range(num+1, len(lista_copy)):
+                if lista_copy[num] > lista_copy[i]:
+                    lista_copy[i], lista_copy[num] = lista_copy[num], lista_copy[i]
     if ordem == 'd':
-        for num in range(0, len(lista)):
-            for i in range(num+1, len(lista)):
-                if lista[num] < lista[i]:
-                    lista[i], lista[num] = lista[num], lista[i]
-    return lista
+        for num in range(0, len(lista_copy)):
+            for i in range(num+1, len(lista_copy)):
+                if lista_copy[num] < lista_copy[i]:
+                    lista_copy[i], lista_copy[num] = lista_copy[num], lista_copy[i]
+    return lista_copy
 
 numeros = []
 while True:
@@ -31,6 +33,6 @@ print('======================================')
 lista_crescente = organizar(numeros, 'c')
 lista_decrescente = organizar(numeros, 'd')
 
-print(f'Lista organizada crescente: {lista_crescente}') #PORQUE AQUI DÁ ERRADO??? AAA ELE ME DA A ORDEM DECRESCENTE
+print(f'Lista organizada crescente: {lista_crescente}')
 print('======================================')
 print(f'Lista organizada decrescente: {lista_decrescente}')
